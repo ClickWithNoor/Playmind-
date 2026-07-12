fea08492ae71fcb74c58f9657446dcf7b1b90308
ClickWithNoor <laibanoo1972@gmail.com>
2026-07-12 15:10:07 +0500

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4dLMGAiPlyksnJNL3b-x1br4N2y6x48U",
+  authDomain: "playmind-d240c.firebaseapp.com",
+  projectId: "playmind-d240c",
+  storageBucket: "playmind-d240c.firebasestorage.app",
+  messagingSenderId: "426265073753",
+  appId: "1:426265073753:web:d19141d4cde972fff83948",
+};
